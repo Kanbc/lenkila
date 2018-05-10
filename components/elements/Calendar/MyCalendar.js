9a6766eea53bba23352dae6b0151636dda9a24a3
Pawarisson <kan.pawarisson@gmail.com>
2018-05-10 18:48:34 +0700

    add event to calendar

--- components/elements/Calendar/MyCalendar.js
+++ components/elements/Calendar/MyCalendar.js
@@ -10,15 +10,12 @@ class MyCalendar extends Component {
         center: 'title',
         right: 'agendaDay,agendaTwoDay, next',
       },
-      businessHours: {
-        // days of week. an array of zero-based day of week integers (0=Sunday)
-        dow: [1, 2, 3, 4, 5], // Monday - Friday
-        start: '07:00', // a start time (10am in this example)
-        end: '26:00', // an end time (6pm in this example)
-      },
       slotWidth: 30,
       editable: true,
       selectable: true,
+      select(startDate, endDate, jsEvent, view, resource) {
+        alert(`selected ${startDate.format()} to ${endDate.format()} on resource ${resource.id}`);
+      },
       droppable: true, // this allows things to be dropped onto the calendar
       drop() {
         // is the "remove after drop" checkbox checked?
@@ -31,6 +28,12 @@ class MyCalendar extends Component {
       // Data Get From API
       minTime: '05:00:00',
       maxTime: '29:00:00',
+      businessHours: {
+        // days of week. an array of zero-based day of week integers (0=Sunday)
+        dow: [1, 2, 3, 4, 5], // Monday - Friday
+        start: '07:00', // a start time (10am in this example)
+        end: '26:00', // an end time (6pm in this example)
+      },
       resourceColumns: [
         {
           labelText: 'สนาม',
@@ -55,6 +58,27 @@ class MyCalendar extends Component {
           id: 'd',
           field: 'D',
         },
+      ],
+      events: [
+        {
+          id: '1',
+          resourceId: 'a',
+          title: 'คุณกานต์',
+          start: '2018-05-10T09:00:00',
+          end: '2018-05-10T10:00:00',
+          color: 'yellow',
+          textColor: 'black',
+        },
+        {
+          id: '2',
+          resourceId: 'a',
+          title: 'คุณเอิท',
+          start: '2018-05-10T09:00:00',
+          end: '2018-05-10T13:00:00',
+          color: 'yellow',
+          textColor: 'black',
+        },
+        // more events here
       ],
     });
   }
