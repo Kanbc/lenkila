b8a77d2385a2b4e4100b8236efc4dfae9f751f1e
Pawarisson <kan.pawarisson@gmail.com>
2018-05-11 16:43:22 +0700

    holiday date form done

--- components/elements/Modal/FMHolidayEditModal.js
+++ components/elements/Modal/FMHolidayEditModal.js
@@ -1,97 +1,116 @@
-import React from 'react';
+import React, { Component } from 'react';
+import DatePicker from 'react-datepicker';
+import moment from 'moment';
 import DefaultModal from './DefaultModal';
 import Body from './DefaultModal/Body';
 import Footer from './DefaultModal/Footer';
-import { Button, Constant, CancelModal } from '../..';
+import { Button, Constant, CancelModal, DatePickerInput } from '../..';
 
-const FMHolidayEditModal = ({ title, type, userData }) => (
-  <DefaultModal title={title} type={type} percentWidth="50" >
-    <Body>
-      <div className="row">
-        <div className="col-sm-2">
-          <p>ชื่อวันหยุด</p>
-        </div>
-        <div className="col-sm-4">
-          <input type="text" className="form-control" id="firstname" defaultValue={userData.firstname} />
-        </div>
-        <div className="col-sm-2">
-          <p>เปิดทำการ</p>
-        </div>
-        <div className="col-sm-4">
-          <input type="text" className="form-control" id="firstname" defaultValue={userData.firstname} />
-        </div>
-      </div>
-      <div className="row">
-        <div className="col-sm-2">
-          <p>วันที่เริ่ม</p>
-        </div>
-        <div className="col-sm-4">
-          <input type="text" className="form-control" id="email" defaultValue={userData.email} />
-        </div>
-        <div className="col-sm-2">
-          <p>เวลาเริ่ม</p>
-        </div>
-        <div className="col-sm-4">
-          <input type="text" className="form-control" id="email" defaultValue={userData.email} />
-        </div>
-      </div>
-      <div className="row">
-        <div className="col-sm-2">
-          <p>วันสิ้นสุด</p>
-        </div>
-        <div className="col-sm-4">
-          <input type="text" className="form-control" id="firstname" defaultValue={userData.email} />
-        </div>
-        <div className="col-sm-2">
-          <p>เวลาสิ้นสุด</p>
-        </div>
-        <div className="col-sm-4">
-          <input type="text" className="form-control" id="firstname" defaultValue={userData.email} />
-        </div>
-      </div>
-    </Body>
-    <Footer>
-      <Button width={Constant.Buttons.default} bstrap="btn-success" >
-        บันทึก
-      </Button>
-      <CancelModal width={Constant.Buttons.default} color={Constant.Orange}>
-        ยกเลิก
-      </CancelModal>
-      <Button width={Constant.Buttons.default} bstrap="btn-danger" >
-        ลบ
-      </Button>
-    </Footer>
-    <style jsx>{`
-      .row{
-        margin-top:10px;
-        margin-bottom:40px;
-        font-size: 1rem;
-        font-weight: 400;
-        line-height: 1.5;
-        color: #212529;
-        text-align: left;
-        p{
-          position: absolute;
-          top: 50%;
-          transform: translateY(-50%);
-          left: 0;
-          right: 0;
-          text-align: center;
-        }
-      }
-      @media (max-width: 576px) { 
-        .row{
-          margin-top:0px;
-          margin-bottom:0px;
-          p{
-            position: relative;
+class FMHolidayEditModal extends Component {
+  state = {
+    currentDate: moment(),
+    currentDate2: moment(),
+  }
+
+  render() {
+    return (
+      <DefaultModal title={this.props.title} type={this.props.type} percentWidth="50" >
+        <Body>
+          <div className="row">
+            <div className="col-sm-2">
+              <p>ชื่อวันหยุด</p>
+            </div>
+            <div className="col-sm-4">
+              <input type="text" className="form-control" id="firstname" defaultValue={this.props.userData.firstname} />
+            </div>
+            <div className="col-sm-2">
+              <p>เปิดทำการ</p>
+            </div>
+            <div className="col-sm-4">
+              <input type="text" className="form-control" id="firstname" defaultValue={this.props.userData.firstname} />
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-sm-2">
+              <p>วันที่เริ่ม</p>
+            </div>
+            <div className="col-sm-4">
+              <DatePicker
+                customInput={<DatePickerInput />}
+                selected={this.state.currentDate}
+                onChange={currentDate => this.setState({ currentDate })}
+              />
+            </div>
+            <div className="col-sm-2">
+              <p>เวลาเริ่ม</p>
+            </div>
+            <div className="col-sm-4">
+              <input type="text" className="form-control" id="email" defaultValue={this.props.userData.email} />
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-sm-2">
+              <p>วันสิ้นสุด</p>
+            </div>
+            <div className="col-sm-4">
+              <DatePicker
+                customInput={<DatePickerInput />}
+                selected={this.state.currentDate2}
+                onChange={currentDate2 => this.setState({ currentDate2 })}
+              />
+            </div>
+            <div className="col-sm-2">
+              <p>เวลาสิ้นสุด</p>
+            </div>
+            <div className="col-sm-4">
+              <input type="text" className="form-control" id="firstname" defaultValue={this.props.userData.email} />
+            </div>
+          </div>
+        </Body>
+        <Footer>
+          <Button width={Constant.Buttons.default} bstrap="btn-success" >
+            บันทึก
+          </Button>
+          <CancelModal width={Constant.Buttons.default} color={Constant.Orange}>
+            ยกเลิก
+          </CancelModal>
+          <Button width={Constant.Buttons.default} bstrap="btn-danger" >
+            ลบ
+          </Button>
+        </Footer>
+        <style jsx>{`
+          .row{
+            margin-top:10px;
+            margin-bottom:40px;
+            font-size: 1rem;
+            font-weight: 400;
+            line-height: 1.5;
+            color: #212529;
             text-align: left;
+            p{
+              position: absolute;
+              top: 50%;
+              transform: translateY(-50%);
+              left: 0;
+              right: 0;
+              text-align: center;
+            }
           }
-        }
-      }
-    `}
-    </style>
-  </DefaultModal>
-);
+          @media (max-width: 576px) { 
+            .row{
+              margin-top:0px;
+              margin-bottom:0px;
+              p{
+                position: relative;
+                text-align: left;
+              }
+            }
+          }
+        `}
+        </style>
+      </DefaultModal>
+    );
+  }
+}
 
 export default FMHolidayEditModal;
