b71497fa655a4caa8d674ad99293daf4f7a80822
Pawarisson <kan.pawarisson@gmail.com>
2019-01-14 16:28:37 +0700

    เพิ่ม text บริการนี้ยังไม่เปิดให้บริการ

--- pages/analysis.js
+++ pages/analysis.js
@@ -6,7 +6,17 @@ import { Layout } from '../components';
 class Analysis extends Component {
   render() {
     return (
-      <Layout title="วิเคราะห์ข้อมูล" />
+      <Layout title="วิเคราะห์ข้อมูล" >
+        <p>บริการนี้ยังไม่เปิดให้บริการ</p>
+        <style jsx>{`
+          p{
+            text-align:center;
+            color: #9B9B9B;
+            margin-top:100px;
+          }
+        `}
+        </style>
+      </Layout>
     );
   }
 }
--- pages/connect.js
+++ pages/connect.js
@@ -4,7 +4,17 @@ import { Layout } from '../components';
 class Connect extends Component {
   render() {
     return (
-      <Layout title="Connect" />
+      <Layout title="Connect">
+        <p>บริการนี้ยังไม่เปิดให้บริการ</p>
+        <style jsx>{`
+          p{
+            text-align:center;
+            color: #9B9B9B;
+            margin-top:100px;
+          }
+        `}
+        </style>
+      </Layout>
     );
   }
 }
