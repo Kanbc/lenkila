70a5a62e414e34320ed8334e2e8c5253f3d4188f
Pawarisson <kan.pawarisson@gmail.com>
2019-04-10 17:15:48 +0700

    fix member and history modal inside edit customer data modal

--- components/elements/Modal/CustEditModal/index.js
+++ components/elements/Modal/CustEditModal/index.js
@@ -43,7 +43,7 @@ class CustEditModal extends Component {
       button2 = <CancelModal width={Constant.Buttons.default} bstrap="btn-danger" onClick={() => this.cancelEditForm()}>ยกเลิก</CancelModal>;
       button3 = null;
     } else {
-      button1 = <ButtonModal color={Constant.Orange} width={Constant.Buttons.default} modalName="#history" > ประวัติ<HistoryModal title="ประวัติ" type="history" history={this.props.history}/></ButtonModal>;
+      button1 = <ButtonModal color={Constant.Orange} width={Constant.Buttons.default} modalName={`#history-${this.props.userData.id}`} > ประวัติ<HistoryModal title="ประวัติ" type={`history-${this.props.userData.id}`} history={this.props.history}/></ButtonModal>;
       button2 = <Button width={Constant.Buttons.default} color={Constant.Blue} bstrap="btn-primary" onClick={() => this.editForm()}>แก้ไข</Button>;
       button3 = <CancelModal width={Constant.Buttons.default} bstrap="btn-danger" onClick={()=>this.props.deleteCustomer(this.props.userData.id)}>ลบ</CancelModal>;
     }
@@ -138,9 +138,9 @@ class CustEditModal extends Component {
               <p>{this.props.userData.customer_type_default}</p>
             </div>
             <div className="col-sm-2">
-              <ButtonModal color={Constant.Orange} width={Constant.Buttons.default} modalName={`#member`}  >
+              <ButtonModal color={Constant.Orange} width={Constant.Buttons.default} modalName={`#member-${this.props.userData.id}`} >
                 Member
-                <MemberModal title="Member" type="member"  userData={this.props.userData} customerType={this.props.customerType} 
+                <MemberModal title="Member" type={`member-${this.props.userData.id}`} userData={this.props.userData} customerType={this.props.customerType} 
                 addCustomerHistory={this.props.addCustomerHistory} 
                 customerTypeId={this.props.customerTypeId}
                 customerTypeIdNext={this.props.customerTypeIdNext}
